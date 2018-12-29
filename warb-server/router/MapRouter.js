eb5accf07c63fae56bc56ee54694f3c370ad0576
DESKTOP-VEPV2L0\jerem <why64951@gmail.com>
2018-12-29 19:08:17 +0700

    update code mới về socket

--- warb-server/router/MapRouter.js
+++ warb-server/router/MapRouter.js
@@ -7,4 +7,7 @@ var router = express.Router();
 router.route('/getLatLng')
 .post(token.checkAccessToken,map.getLatLong)
 
+router.route('/getArrayLocation')
+.post(token.checkAccessToken,map.getArrayLocation)
+
 module.exports = router;
