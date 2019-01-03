04890aa28203e92af2248cb37eba921e13319b0f
haolk <haolk@vng.com.vn>
2019-01-03 09:13:24 +0700

    deploy web to heruku

--- warb-server/app.js
+++ warb-server/app.js
@@ -13,6 +13,32 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('*', function (req, res, next) {
+    res.sendFile(path.join(__dirname + '/build/index.html'));
+});
+
+
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
+
 // thiet lap router
 app.use('/user', require('./router/UserRouter.js'));
 app.use('/refreshToken', require('./router/TokenRouter.js'));
---
+++ warb-server/build/precache-manifest.a903008beba37ae270f67073425771e9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2503169017014036cf0dd0dd7c2a2b8a",
+    "url": "/static/media/sidebar-3.25031690.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6e9f2cc2fd0bbe967793",
+    "url": "/static/js/main.6e9f2cc2.chunk.js"
+  },
+  {
+    "revision": "3a78d025ffd9b17f3a13",
+    "url": "/static/js/1.3a78d025.chunk.js"
+  },
+  {
+    "revision": "6e9f2cc2fd0bbe967793",
+    "url": "/static/css/main.a586ab34.chunk.css"
+  },
+  {
+    "revision": "3a78d025ffd9b17f3a13",
+    "url": "/static/css/1.e8476bbe.chunk.css"
+  },
+  {
+    "revision": "652ec13f4b2ea99a89f75e3b99674907",
+    "url": "/index.html"
+  }
+];
